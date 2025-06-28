c32679bfa9ab87a32fa54707870854716b991252
Michael Brown <mikey.be@gmail.com>
2025-06-28 23:11:43 +0100

    Update test.f
    Added tests for divide, modulus, and multiply
    Fixed fib (the first 2 are pre populated 0 1, need to sub 2 for the rest)

--- test.f
+++ test.f
@@ -1,13 +1,26 @@
 : ten 10 ;
 : fourtytwo ten 32 + ;
 : iflooptest 77 0 IF 3 0 DO ten + LOOP ELSE fourtytwo + THEN .;
+: testdiv 33 4 / . ;
+: testmod 33 4 % . ;
+: testmul 33 4 * . ;
 
 : fib
 	VAR c 
-	c ! 
+	2 -
+	c !
 	0 DUP . 1 DUP . 
 	c @ 0 DO 
 		OVER OVER  + DUP . 
 	LOOP ;
 
-: main 10 fib 0;
+: main 
+	iflooptest 
+	
+	testdiv
+	testmod
+	testmul
+	
+	10 fib 
+	
+	0;
